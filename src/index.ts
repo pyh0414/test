a34bef5bd72d4327bb230256e96c5d20d608a2d8
yeonhoPark <whwlsvy12@naver.com>
2023-08-03 16:46:25 +0900

    temp12

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
 import mysql from "mysql2/promise";
 
 export const entryPoint = async (req, res) => {
-  console.log(process.env);
-
-  try {
-    console.log(process.env);
-
-    const connection = await mysql.createConnection({
-      socketPath: process.env.INSTANCE_UNIX_SOCKET,
-      port: 3306,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-    });
-
-    await connection.connect();
-
-    const result = await connection.query("select * from User");
-    console.log(result);
-  } catch (err) {
-    console.log(err);
-  }
+  console.log(new Date());
+  console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+  console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 
   return res.send("entryPoint!");
 };
